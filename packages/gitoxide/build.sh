a885b9c77c80515d38d77a5041500c147e77d670
Twaik Yont <twaikyont@gmail.com>
2023-12-07 15:37:34 +0200

    bump(main/gitoxide): 0.32.0 Fixes #18642

--- packages/gitoxide/build.sh
+++ packages/gitoxide/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="Rust implementation of Git"
 TERMUX_PKG_LICENSE="Apache-2.0, MIT"
 TERMUX_PKG_LICENSE_FILE="LICENSE-APACHE, LICENSE-MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="0.31.1"
+TERMUX_PKG_VERSION="0.32.0"
 TERMUX_PKG_SRCURL=https://github.com/Byron/gitoxide/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=639c366d3767f5391e055a985de0ac9142fd56f76a1920bacd920b25dabc3b64
+TERMUX_PKG_SHA256=5a17da0379254bd996fe1888de4104d551a41bdd8bd4b93034f9d0757382fa75
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+\.\d+\.\d+"
 TERMUX_PKG_DEPENDS="resolv-conf"
@@ -18,8 +18,11 @@ termux_step_pre_configure() {
 
 	: "${CARGO_HOME:=$HOME/.cargo}"
 	export CARGO_HOME
-	export ANDROID_NDK_HOME=$NDK
-	echo ANDROID_NDK_HOME=$ANDROID_NDK_HOME
+	if [[ "$TERMUX_ARCH" == "x86_64" ]]; then
+		echo " -L$(dirname $(find $NDK -name libclang_rt.builtins-x86_64-android.a))"
+		RUSTFLAGS+=" -L$(dirname $(find $NDK -name libclang_rt.builtins-x86_64-android.a))"
+		sed  -i '/fn main() {/a    println!("cargo:rustc-link-lib=static=clang_rt.builtins-x86_64-android");' $TERMUX_PKG_SRCDIR/build.rs
+	fi
 
 	cargo fetch --target "${CARGO_TARGET_NAME}"
 
