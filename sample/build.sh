bd176121ab5ad44ab9030a1aea7517c956955460
Harry Weinstein <449154+bigbio2002@users.noreply.github.com>
2024-04-05 20:35:18 -0400

    update default build.sh template

--- sample/build.sh
+++ sample/build.sh
@@ -2,12 +2,16 @@
 # For reference about available fields, check the Termux Developer's Wiki page:
 # https://github.com/termux/termux-packages/wiki/Creating-new-package
 
-TERMUX_PKG_HOMEPAGE=
+TERMUX_PKG_HOMEPAGE=https://github.com/
 TERMUX_PKG_DESCRIPTION=""
 TERMUX_PKG_LICENSE=""
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=
+TERMUX_PKG_VERSION=""
+#TERMUX_PKG_REVISION=
 TERMUX_PKG_SRCURL=
 TERMUX_PKG_SHA256=
 #TERMUX_PKG_DEPENDS=""
+#TERMUX_PKG_BREAKS=""
+#TERMUX_PKG_REPLACES=""
 #TERMUX_PKG_BUILD_IN_SRC=true
+#TERMUX_PKG_EXTRA_CONFIGURE_ARGS=""
