60f15c34e602b83dda3b29eb24c1bb8c23513778
Harry Weinstein <449154+bigbio2002@users.noreply.github.com>
2024-04-11 01:09:28 -0400

    pinfo: working to add new package (incomplete)

---
+++ packages/pinfo/build.sh
@@ -0,0 +1,13 @@
+# Skeleton build.sh script for new package.
+# For reference about available fields, check the Termux Developer's Wiki page:
+# https://github.com/termux/termux-packages/wiki/Creating-new-package
+
+TERMUX_PKG_HOMEPAGE=https://github.com/baszoetekouw/pinfo
+TERMUX_PKG_DESCRIPTION="Console-based info reader"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION=0.6.13
+TERMUX_PKG_SRCURL=
+TERMUX_PKG_SHA256=
+TERMUX_PKG_DEPENDS="ncurses"
+#TERMUX_PKG_BUILD_IN_SRC=true
