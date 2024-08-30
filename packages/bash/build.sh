39237d2b2c78b475d2e9f1bf8f1a052389a28af8
Harry Weinstein <449154+bigbio2002@users.noreply.github.com>
2024-08-30 06:05:10 -0400

    bash/libxext: disable missing Bionic funcs from configure

--- packages/bash/build.sh
+++ packages/bash/build.sh
@@ -30,7 +30,8 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" bash_cv_dev_fd=whacky"
 # - http://permalink.gmane.org/gmane.linux.embedded.yocto.general/25204
 # - https://github.com/termux/termux-app/issues/200
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" bash_cv_getcwd_malloc=yes"
-
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" ac_cv_func_getrandom=no"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" ac_cv_func_getentropy=no"
 TERMUX_PKG_CONFFILES="etc/bash.bashrc etc/profile"
 
 TERMUX_PKG_RM_AFTER_INSTALL="share/man/man1/bashbug.1 bin/bashbug"
--- packages/libxext/build.sh
+++ packages/libxext/build.sh
@@ -6,6 +6,9 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.3.6"
 TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/lib/libXext-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=edb59fa23994e405fdc5b400afdf5820ae6160b94f35e3dc3da4457a16e89753
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-malloc0returnsnull"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+ac_cv_func_reallocarray=no
+--enable-malloc0returnsnull
+"
 TERMUX_PKG_DEPENDS="libx11"
 TERMUX_PKG_BUILD_DEPENDS="xorgproto, xorg-util-macros"
