f825972e3ccbba7cf1dd850ae657480e662d9f1e
Harry Weinstein <449154+bigbio2002@users.noreply.github.com>
2024-09-17 20:14:42 -0400

    fluidsynth: added sdl2 dependency

--- packages/fluidsynth/build.sh
+++ packages/fluidsynth/build.sh
@@ -7,7 +7,7 @@ TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/FluidSynth/fluidsynth/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=3340d73286b28fe6e5150fbe12648d4640e86c64c228878b572773bd08cac531
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="dbus, glib, libc++, libsndfile, pulseaudio, readline"
+TERMUX_PKG_DEPENDS="dbus, glib, libc++, sdl2, libsndfile, pulseaudio, readline"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DLIB_INSTALL_DIR=${TERMUX_PREFIX}/lib
 "
