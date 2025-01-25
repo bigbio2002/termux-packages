40e86ef964c92b21e57b0a98c2f3a5e5af5034c4
Harry Weinstein <449154+bigbio2002@users.noreply.github.com>
2025-01-25 18:11:25 -0500

    libmpeg2: add libandroid-shmem to LDFLAGS

--- packages/libmpeg2/build.sh
+++ packages/libmpeg2/build.sh
@@ -7,5 +7,6 @@ TERMUX_PKG_SRCURL=https://libmpeg2.sourceforge.io/files/libmpeg2-${TERMUX_PKG_VE
 TERMUX_PKG_SHA256=dee22e893cb5fc2b2b6ebd60b88478ab8556cb3b93f9a0d7ce8f3b61851871d4
 
 termux_step_pre_configure() {
+	LDFLAGS+=" -landroid-shmem"
 	autoreconf -vfi
 }
