e1e408bc6ef8a71e9f47e352071fe656e302daf7
Harry Weinstein <449154+bigbio2002@users.noreply.github.com>
2025-03-01 04:40:56 -0500

    luarocks: point configure to lua5.1-specific include dir

--- packages/luarocks/build.sh
+++ packages/luarocks/build.sh
@@ -22,7 +22,8 @@ termux_step_configure() {
 	fi
 
 	./configure --prefix="$TERMUX_PREFIX" \
-		--with-lua="$TERMUX_PREFIX"
+		--with-lua="$TERMUX_PREFIX" \
+		--with-lua-include="$TERMUX_PREFIX/include/lua$__LUA_VERSION"
 }
 
 termux_step_post_make_install() {
