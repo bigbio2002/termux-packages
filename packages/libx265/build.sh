0841abae0496852b59166591e610ba001b2a48cd
Harry Weinstein <449154+bigbio2002@users.noreply.github.com>
2025-10-11 21:14:54 -0400

    libx265: bump soname

--- packages/libx265/build.sh
+++ packages/libx265/build.sh
@@ -9,30 +9,11 @@ TERMUX_PKG_SHA256=a31699c6a89806b74b0151e5e6a7df65de4b49050482fe5ebf8a4379d7af8f
 TERMUX_PKG_DEPENDS="libandroid-posix-semaphore, libc++"
 TERMUX_PKG_BREAKS="libx265-dev"
 TERMUX_PKG_REPLACES="libx265-dev"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
--DCMAKE_POLICY_VERSION_MINIMUM=3.5
-"
 
-termux_step_post_get_source() {
-	git fetch --unshallow
-	git checkout $_COMMIT
-
-	local pdate="p$(git log -1 --format=%cs | sed 's/-//g')"
-	if [[ "$TERMUX_PKG_VERSION" != *"${pdate}" ]]; then
-		echo -n "ERROR: The version string \"$TERMUX_PKG_VERSION\" is"
-		echo -n " different from what is expected to be; should end"
-		echo " with \"${pdate}\"."
-		return 1
-	fi
-
-	local s=$(find . -type f ! -path '*/.git/*' -print0 | xargs -0 sha256sum | LC_ALL=C sort | sha256sum)
-	if [[ "${s}" != "${TERMUX_PKG_SHA256}  "* ]]; then
-		termux_error_exit "Checksum mismatch for source files."
-	fi
-
+termux_step_pre_configure() {
 	# Do not forget to bump revision of reverse dependencies and rebuild them
 	# after SOVERSION is changed.
-	local _SOVERSION=207
+	local _SOVERSION=215
 
 	local v=$(sed -En 's/^.*set\(X265_BUILD ([0-9]+).*$/\1/p' \
 			source/CMakeLists.txt)
@@ -40,24 +21,6 @@ termux_step_post_get_source() {
 		termux_error_exit "SOVERSION guard check failed."
 	fi
 
-	if [ -d .git ]; then
-		local _libx265_base_version="3.5"
-		local _libx265_base_commit="f0c1022b6be121a753ff02853fbe33da71988656"
-		if [ "${TERMUX_PKG_VERSION%-*}" != "${_libx265_base_version}" ]; then
-			termux_error_exit "Base version mismatch; expected to be ${_libx265_base_version}."
-		fi
-		cat > x265Version.txt <<-EOF
-			repositorychangeset: $(git log --pretty=format:%h -n 1)
-			releasetagdistance: $(git rev-list ${_libx265_base_commit}.. --count --first-parent)
-			releasetag: ${_libx265_base_version}
-		EOF
-
-		# To install shared lib
-		rm -rf .git
-	fi
-}
-
-termux_step_pre_configure() {
 	local _TERMUX_CLANG_TARGET=
 
 	# Not sure if this is necessary for on-device build
@@ -68,7 +31,7 @@ termux_step_pre_configure() {
 
 	if [ "$TERMUX_ARCH" = arm ] || [ "$TERMUX_ARCH" = i686 ]; then
 		# Avoid text relocations and/or build failure.
-		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+="-DENABLE_ASSEMBLY=OFF"
+		TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DENABLE_ASSEMBLY=OFF"
 	fi
 
 	TERMUX_PKG_SRCDIR="$TERMUX_PKG_SRCDIR/source"
