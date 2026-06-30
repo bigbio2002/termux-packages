a77b6806679855a4576e4d114baf88560aa2f3a5
Harry Weinstein <449154+bigbio2002@users.noreply.github.com>
2026-06-30 10:37:03 -0400

    new: gpg-tui

--- packages/gpg-tui/build.sh
+++ packages/gpg-tui/build.sh
@@ -10,6 +10,23 @@ TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
 	termux_setup_rust
+
+	cargo vendor
+	find ./vendor \
+		-mindepth 1 -maxdepth 1 -type d \
+		! -wholename ./vendor/arboard \
+		! -wholename ./vendor/x11rb-protocol \
+		-exec rm -rf '{}' \;
+
+	find vendor/{arboard,x11rb-protocol} -type f -print0 | \
+		xargs -0 sed -i \
+		-e 's|android|disabling_this_because_it_is_for_building_an_apk|g' \
+		-e "s|/tmp|$TERMUX_PREFIX/tmp|g"
+
+	echo "" >> Cargo.toml
+	echo '[patch.crates-io]' >> Cargo.toml
+	echo "arboard = { path = \"./vendor/arboard\" }" >> Cargo.toml
+	echo "x11rb-protocol = { path = \"./vendor/x11rb-protocol\" }" >> Cargo.toml
 }
 
 termux_step_post_make_install() {
